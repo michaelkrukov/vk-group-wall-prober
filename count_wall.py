968dedd523eca8fdb9b597ddb4e3cd4c7d81e540
Michael Krukov <krukov.michael@ya.ru>
2018-07-09 23:50:38 +0300

    added support for python3; new metrics

--- count_wall.py
+++ count_wall.py
@@ -14,7 +14,7 @@ from copy import deepcopy
 # Fill here
 TOKEN = ""
 GROUP_ID = ""
-URL = f"https://api.vk.com/method/{{key}}?access_token={TOKEN}&v=5.74"
+URL = "https://api.vk.com/method/{{key}}?access_token={}&v=5.74".format(TOKEN)
 
 COUNT_STEP = 100
 EXECUTE_SIZE = 25
@@ -42,15 +42,15 @@ def req(key, **params):
     return j["response"]
 
 def executable(key, **params):
-    result = f"API.{key}({{"
+    result = "API.{}({{".format(key)
 
     for k, v in params.items():
-        result += f'"{k}": '
+        result += '"{}": '.format(k)
 
         if isinstance(v, str):
             result += '"' + v.replace("\\", "\\\\").replace('"', '\\"').replace("\n", "<br>") + '"'
         elif isinstance(v, int):
-            result += f'{v}'
+            result += str(v)
         else:
             continue
 
@@ -77,28 +77,36 @@ def collect_profiles(collection, profiles):
         collection[p["id"]] = {
             "name": p.get("first_name", "?") + " " +
                 p.get("last_name", "?") + " (vk.com/id" + str(p["id"]) + ")",
-            "likes_day": 0, "likes_week": 0
+            "likes_day": 0, "likes_week": 0, "likes_month": 0
         }
 
 def main():
     # Some constants
     day_ago = time.time() - 24 * 60 * 60
     week_ago = time.time() - 7 * 24 * 60 * 60
+    month_ago = time.time() - 4 * 7 * 24 * 60 * 60
 
     print("> script is started...")
 
     # Test for work and receiving "count"
     wall = req("wall.get", owner_id="-" + str(GROUP_ID), count=COUNT_STEP)
 
+    owner_only_wall = req("wall.get", owner_id="-" + str(GROUP_ID), count=COUNT_STEP, filter="owner")
+
     count = wall["count"]
+    owner_count = owner_only_wall["count"]
+    others_count = count - owner_count
 
     profiles = {}
     result = {
-        "amounts": {"day": 0, "week": 0, "all": count}
+        "amounts": {"day": 0, "week": 0, "month": 0, "all": count},
+        "amounts_others_only": {"day": 0, "week": 0, "month": 0, "all": others_count},
+        "amounts_owner_only": {"day": 0, "week": 0, "month": 0, "all": owner_count}
     }
 
     day_comments = []
     week_comments = []
+    month_comments = []
 
     current_offset = 0
     current_requests = []
@@ -122,17 +130,55 @@ def main():
         post_max_likes_count = -1
         post_max_likes = ""
 
-        print(f"> getting and processing posts up to {current_offset}...")
+        post_max_likes_count_week = -1
+        post_max_likes_week = ""
+
+        print("> getting and processing posts up to {}...".format(current_offset))
 
         for res in execute(current_requests):
             for post in res["items"]:
                 if post["date"] >= day_ago:
                     result["amounts"]["day"] += 1
                     result["amounts"]["week"] += 1
+                    result["amounts"]["month"] += 1
+
+                    if post["from_id"] == post["owner_id"]:
+                        result["amounts_owner_only"]["day"] += 1
+                        result["amounts_owner_only"]["week"] += 1
+                        result["amounts_owner_only"]["month"] += 1
+                    else:
+                        result["amounts_others_only"]["day"] += 1
+                        result["amounts_others_only"]["week"] += 1
+                        result["amounts_others_only"]["month"] += 1
+
                     day_comments.append([post["owner_id"], post["id"], 0, "day"])
+
                 elif post["date"] >= week_ago:
                     result["amounts"]["week"] += 1
+                    result["amounts"]["month"] += 1
+
+                    if post["from_id"] == post["owner_id"]:
+                        result["amounts_owner_only"]["week"] += 1
+                        result["amounts_owner_only"]["month"] += 1
+                    else:
+                        result["amounts_others_only"]["week"] += 1
+                        result["amounts_others_only"]["month"] += 1
+
                     week_comments.append([post["owner_id"], post["id"], 0, "week"])
+
+                    if post["likes"]["count"] >= post_max_likes_count_week:
+                        post_max_likes_count_week = post["likes"]["count"]
+                        post_max_likes_week = "vk.com/wall" + str(post["owner_id"]) + \
+                            "_" + str(post["id"])
+                elif post["date"] >= month_ago:
+                    result["amounts"]["month"] += 1
+
+                    if post["from_id"] == post["owner_id"]:
+                        result["amounts_owner_only"]["month"] += 1
+                    else:
+                        result["amounts_others_only"]["month"] += 1
+
+                    month_comments.append([post["owner_id"], post["id"], 0, "month"])
                 else:
                     posts_ready = True
                     continue
@@ -143,11 +189,12 @@ def main():
                         "_" + str(post["id"])
 
         result["max_likes_post"] = post_max_likes or "Пусто"
+        result["max_likes_post_week"] = post_max_likes_week or "Пусто"
 
-    print(f"> getting and processing comments...")
+    print("> getting and processing comments...")
 
     new_packs = []
-    packs = day_comments + week_comments
+    packs = day_comments + week_comments + month_comments
 
     while packs:
         current_requests.clear()
@@ -175,6 +222,9 @@ def main():
                     if pack[3] in ("day", "week"):
                         prof["likes_week"] += item["likes"]["count"]
 
+                    if pack[3] in ("day", "week", "month"):
+                        prof["likes_month"] += item["likes"]["count"]
+
                 if pack[2] + COUNT_STEP < res["count"]:
                     new_packs.append([pack[0], pack[1], pack[2] + COUNT_STEP, pack[3]])
 
@@ -185,6 +235,7 @@ def main():
 
     result["top_commentors_day"] = sorted(profiles.values(), key=lambda x: -x["likes_day"])[:3]
     result["top_commentors_week"] = sorted(profiles.values(), key=lambda x: -x["likes_week"])[:3]
+    result["top_commentors_month"] = sorted(profiles.values(), key=lambda x: -x["likes_month"])[:3]
 
     return result
 
@@ -196,6 +247,10 @@ if __name__ == "__main__":
 
     print("Group id:", GROUP_ID)
     print("Posts:\n", result["amounts"])
-    print("Most liked posts:\n", result["max_likes_post"])
+    print("Posts from owner:\n", result["amounts_owner_only"])
+    print("Posts from others:\n", result["amounts_others_only"])
+    print("Most liked posts for last week:\n", result["max_likes_post_week"])
+    print("Most liked posts for last month:\n", result["max_likes_post"])
     print("Top commentors for a day:\n", result["top_commentors_day"])
     print("Top commentors for a week:\n", result["top_commentors_week"])
+    print("Top commentors for a month:\n", result["top_commentors_month"])
